981eb8244db8d328c79ee7184f4aa70800bc8409
Rajesh Saini <saini222302@gmail.com>
2023-04-09 15:31:17 +0530

    PoftFolio

--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -1,6 +1,18 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
+import html from '../../Assets/assets/svg/skills/html.svg'
+import css from '../../Assets/assets/svg/skills/css.svg'
+import javascript from '../../Assets/assets/svg/skills/javascript.svg'
+import react from '../../Assets/assets/svg/skills/react.svg'
+import figma from '../../Assets/assets/svg/skills/figma.svg'
+import bootstrap from '../../Assets/assets/svg/skills/bootstrap.svg'
+import mysql from '../../Assets/assets/svg/skills/mysql.svg'
+import mongoDB from '../../Assets/assets/svg/skills/mongoDB.svg'
+import tailwind from '../../Assets/assets/svg/skills/tailwind.svg'
+import typescript from '../../Assets/assets/svg/skills/typescript.svg'
+import materialui from '../../Assets/assets/svg/skills/materialui.svg'
+
 import {
   DiJavascript1,
   DiReact,
@@ -17,38 +29,43 @@ import {
 
 function Techstack() {
   return (
+    <>
+    
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
+        {/* <CgCPlusPlus /> */}
+        <img src={html}  style={{ height: "80px"}} alt="no"/><h3>html</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
+        {/* <DiJavascript1 /> */}
+        <img src={css}  style={{ height: "80px"}} alt="no"/><h3>CSS</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
+      <img src={javascript}  style={{ height: "80px"}} alt="no"/><h3>javascript</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
+      <img src={react}  style={{ height: "80px"}} alt="no"/><h3>React</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
+      <img src={bootstrap}  style={{ height: "80px"}} alt="no"/><h3>bootstrap</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
+      <img src={mysql}  style={{ height: "80px"}} alt="no"/><h3>mysql</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
+      <img src={mongoDB}  style={{ height: "80px"}} alt="no"/><h3>mongoDB</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
+      <img src={tailwind}  style={{ height: "80px"}} alt="no"/><h3>tailwind</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
+      <img src={typescript}  style={{ height: "80px"}} alt="no"/><h3>typescript</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPytorch />
+      <img src={materialui}  style={{ height: "80px"}} alt="no"/><h3>materialui</h3>
       </Col>
     </Row>
+    </>
   );
 }
 
